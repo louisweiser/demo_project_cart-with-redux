d8ac709b811912110c79c52587af86ce87ea7844
Louis Weiser <louis.weiser98@gmail.com>
2023-08-10 18:04:34 +0200

    send http request by using useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
+const apiUrl = process.env.REACT_APP_API_URL;
+
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(apiUrl, {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
